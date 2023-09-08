49d41ff60b20de1a13d5b267e40a6188356c8db7
Sebastian <a20206438@pucp.edu.pe>
2023-09-07 20:21:59 -0500

    lab04

---
+++ Lab04_20206438/src/main/java/com/example/lab04_20206438/Lab0420206438Application.java
@@ -0,0 +1,13 @@
+package com.example.lab04_20206438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0420206438Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab0420206438Application.class, args);
+    }
+
+}
---
+++ Lab04_20206438/src/test/java/com/example/lab04_20206438/Lab0420206438ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab04_20206438;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0420206438ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
